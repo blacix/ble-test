186d1bad2924eb6b88ceb7008a8b51743ef6307a
Laszlo Bartok <bartok@nowtech.hu>
2023-08-19 17:35:26 +0200

    large msgs

--- ble_peripheral.py
+++ ble_peripheral.py
@@ -22,15 +22,15 @@ TEST_MESSAGE_SIZE = 2048
 class PeripheralTest:
     def __init__(self) -> None:
         self.rx_buffer = []
+        self.rx_string = ""
         self.tx_buffer = []
         self.test_device_peripheral: peripheral.Peripheral = None
         self.rx_characteristic: localGATT.Characteristic = None
         self.tx_characteristic: localGATT.Characteristic = None
-        self.rx_value: int = 0
         self.MTU = 20
         self.tx_str = ""
         for i in range(0, TEST_MESSAGE_SIZE - 1):
-            self.tx_str += str(i % 10)
+            self.tx_str += '+'
         self.tx_str += '\n'
         print(self.tx_str)
 
@@ -42,14 +42,20 @@ class PeripheralTest:
         return self.tx_buffer
 
     def rx_write_callback(self, value: [], options):
-        print('rx_write_callback, options: ', options)
+        print('rx_write_callback, options: ', options, 'value', value)
         # minus ATT header and l2cap header
         self.MTU = options['mtu'] - 2 - 3
         print('MTU: ', self.MTU)
-        self.rx_buffer = value
-        self.rx_value = int.from_bytes(self.rx_buffer, byteorder='big')
-        print('rx_write_callback', self.rx_value)
-        self.update_tx_value(self.tx_characteristic)
+        self.rx_string += value.decode('utf-8')
+        rx_strings = self.rx_string.split('\n')
+        if len(rx_strings) > 1:
+            for s in rx_strings[:-1]:
+                self.on_message_received(s)
+            self.rx_string = rx_strings[-1]
+
+    def on_message_received(self, message):
+        print("on_message_received", message)
+        self.update_tx_value(self.tx_characteristic, message + '\n')
 
     def on_connect(self, local_address: device.Device = None, remote_address=None):
         print('on_connect', local_address, remote_address)
@@ -57,27 +63,20 @@ class PeripheralTest:
     def on_disconnect(self, local_address=None, remote_address=None):
         print('on_disconnect', local_address, remote_address)
 
-    def update_tx_value(self, characteristic: localGATT.Characteristic):
-        """
-        callback to send notifications
-        :param characteristic:
-        :return: boolean to indicate if timer should continue
-        """
+    def update_tx_value(self, characteristic: localGATT.Characteristic, message: str):
         # Causes characteristic to be updated and send notification
         print('update_tx_value')
-        self.rx_value = self.rx_value + 1
-        self.tx_buffer = self.rx_value.to_bytes(4, byteorder='big')
-        self.tx_buffer = bytearray(self.tx_str, 'utf-8')
+        self.tx_buffer = bytearray(message, 'utf-8')
         offset = 0
         tx_buffer_len = len(self.tx_buffer)
         while offset < tx_buffer_len:
             out = self.tx_buffer[offset:offset + self.MTU]
-            print('chunk', out)
+            # print('chunk', out)
             characteristic.set_value(out)
             offset += self.MTU
         if offset < tx_buffer_len:
             out = self.tx_buffer[offset:]
-            print('chunk', out)
+            # print('chunk', out)
             characteristic.set_value(out)
 
         # Return True to continue notifying. Return a False will stop notifications
@@ -108,7 +107,7 @@ class PeripheralTest:
         # Add characteristic
         self.rx_characteristic = localGATT.Characteristic(service_id=1, characteristic_id=1, uuid=RX_CHARACTERISTIC,
                                                           value=[], notifying=False,
-                                                          flags=['write'],
+                                                          flags=['write', 'reliable-write'],
                                                           write_callback=self.rx_write_callback,
                                                           read_callback=None,
                                                           notify_callback=None)
